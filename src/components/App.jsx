356e0cafa219b5c1a655fe845a09986f533f53eb
SoorajModi <soomodi@gmail.com>
2020-12-27 16:13:38 -0500

    Add Header.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/style.css';
+
+import Header from './Header';
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <h1>Hello world!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/test/Header.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+test("should render title of application", () => {
+    render(<Header />);
+    const linkElement = screen.getByText(/Citation Manager/i);
+    expect(linkElement).toBeInTheDocument();
+});
