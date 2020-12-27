5b52704d69f5c4b0b0fcea946d0bb47b3122db5b
SoorajModi <soomodi@gmail.com>
2020-12-27 18:00:41 -0500

    Add Footer.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import '../css/style.css';
 
 import Header from './Header';
 import HomePage from './HomePage'
+import Footer from "./Footer";
 
 function App() {
     return (
         <div>
             <Header />
             <HomePage />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>About</p>
+            <p>Contact</p>
+            <p>Resources</p>
+            <p>Code</p>
+        </footer>
+    );
+}
+
+export default Footer;
