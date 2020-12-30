51fb58577aa9f95a79a6618755cb04f215dc77ce
SoorajModi <soomodi@gmail.com>
2020-12-30 13:21:02 -0500

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import {
   Route,
 } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/test">
-          <h1>Test</h1>
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/test">
+            <h1>Test</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import '../css/homePage.css';
 import HomeTitle from './HomeTitle';
-import Header from './Header';
-import Footer from './Footer';
 
 function Home() {
   return (
-    <div>
-      <Header />
-      <div className="home">
-        <HomeTitle />
-      </div>
-      <Footer />
+    <div className="home">
+      <HomeTitle />
     </div>
   );
 }
